2c818d22259c58c08275a9f9c074e5fd717de984
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-05-03 11:17:52 +0200

    Update docusaurus settings about image in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/gpt-ai-flow-social-card.jpg',
       navbar: {
         title: 'GPT AI Flow',
         logo: {
