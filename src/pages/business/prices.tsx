83fe54aed642dca20ea1e83999ddec990ad2f444
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-08-03 22:13:00 +0200

    Update prices page from test to live

--- src/pages/business/prices.tsx
+++ src/pages/business/prices.tsx
@@ -18,8 +18,8 @@ function PricingPage() {
     <div className="business_prices_page_contianer">
       {/* @ts-ignore */}
       <stripe-pricing-table
-        pricing-table-id="prctbl_1Na0w5DfpKDtUNgnp36d0q4l"
-        publishable-key="pk_test_51HqdzoDfpKDtUNgns7gfUm2rLbJAqgKVTiJVBpkX2ViLKkmLiV11eXasYcJjUsj6ARpDhju4yAFMiWAEl3ynE2T100S1mBLctz"
+        pricing-table-id="prctbl_1Nb7qtDfpKDtUNgnaWDOfapE"
+        publishable-key="pk_live_51HqdzoDfpKDtUNgnMQxyO3NPLAxBb1IS53DE9YXgmV9w5rEwsAOBaohqyL7bNTVjZQ8N4Ry0R9XXDIW9q0aMZFk100OtkFDHRx"
       />
     </div>
   );
