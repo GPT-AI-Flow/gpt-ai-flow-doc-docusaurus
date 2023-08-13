0322e0664f612f57b781c2ff09f1bd6a15ba4eee
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-08-13 17:09:38 +0200

    Clean

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -93,27 +93,6 @@ function Feature(props: FeatureItem) {
   );
 }
 
-const people = [
-  {
-    name: 'Calvin Hawkins',
-    email: 'calvin.hawkins@example.com',
-    image:
-      'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Kristen Ramos',
-    email: 'kristen.ramos@example.com',
-    image:
-      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Ted Fox',
-    email: 'ted.fox@example.com',
-    image:
-      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-];
-
 export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
@@ -122,34 +101,6 @@ export default function HomepageFeatures(): JSX.Element {
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
-        </div>
-        <div className="row">
-          {' '}
-          <ul className="divide-y divide-gray-200">
-            {people.map((person) => (
-              <li key={person.email} className="py-4 flex">
-                <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
-                <div className="ml-3">
-                  <p className="text-sm font-medium text-gray-900">{person.name}</p>
-                  <p className="text-sm text-gray-500">{person.email}</p>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="row">
-          <header className="bg-blue-500">
-            <div className="container mx-auto text-center py-24">
-              <h1 className="text-4xl font-bold text-white">{'siteConfig.title'}</h1>
-              <p className="text-xl py-6 text-white">{'siteConfig.tagline'}</p>
-
-              <div className="py-10">
-                <Link className="bg-white rounded-md text-gray-500 px-4 py-2" to="/docs/intro">
-                  Docusaurus Tutorial - 5min ⏱️
-                </Link>
-              </div>
-            </div>
-          </header>
         </div>
       </div>
     </section>
