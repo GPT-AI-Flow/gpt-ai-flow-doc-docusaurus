a0baaaa11e8c86529834aa853abc8af7407dd660
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-08-14 00:24:29 +0200

    Install and setup sass and docusaurus-plugin-sass

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -110,7 +110,7 @@ const config = {
           //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve('./src/css/custom.scss'),
         },
         sitemap: {
           changefreq: 'weekly',
@@ -283,6 +283,7 @@ const config = {
         },
       };
     },
+    'docusaurus-plugin-sass',
   ],
 };
 
