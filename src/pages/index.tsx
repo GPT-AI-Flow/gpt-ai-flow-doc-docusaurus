415e3dfc4c0b420b5b7cb56ccb0b091b86227ea6
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-08-14 00:38:35 +0200

    Demo 4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 // import Stats from '../astro-theme/components/Stats.astro';
 // import Features from '../astro-theme/components/Features.astro';
-// import HeroSection from '../astro-theme/components/HeroSection.astro';
+import HeroSection from './astro-theme/components/HeroSection';
 import Layout from './astro-theme/layouts/Layout';
 // import Testimonials from '../astro-theme/components/Testimonials.astro';
 // import CallToAction from '../astro-theme/components/CallToAction.astro';
@@ -14,8 +14,8 @@ export default function Home(): JSX.Element {
   return (
     <Layout title="Welcome to Astrolus.">
       <main className="space-y-40 mb-40">
-        {/* <HeroSection />
-        <Features />
+        <HeroSection />
+        {/* <Features />
         <Stats />
         <Testimonials />
         <CallToAction />
