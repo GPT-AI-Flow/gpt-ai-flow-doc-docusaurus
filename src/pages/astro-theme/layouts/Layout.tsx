fe7df654fce3ee8fe324871f6f5dccb94f5901e4
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-08-14 00:02:30 +0200

    Demo blank

---
+++ src/pages/astro-theme/layouts/Layout.tsx
@@ -0,0 +1,35 @@
+import './Layout.css';
+
+import React from 'react';
+import Head from '@docusaurus/Head';
+
+// import AppFooter from '../components/AppFooter.astro';
+// import AppHeader from '../components/AppHeader.tsx';
+
+const Layout = (props: any) => {
+  return (
+    <>
+      <Head>
+        <html lang="zh" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        {/* <meta name="generator" content={Astro.generator} /> */}
+        <meta name="description" content="Template built with tailwindcss using Tailus blocks v2" />
+        <title>Tailus astro theme</title>
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="bg-white dark:bg-gray-900">
+        {/* <AppHeader /> */}
+        <slot />
+        {/* <AppFooter /> */}
+      </body>
+    </>
+  );
+};
+
+export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,25 @@
 import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/intro">
-            立刻使用 AI 工具成为自媒体博主
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+// import Stats from '../astro-theme/components/Stats.astro';
+// import Features from '../astro-theme/components/Features.astro';
+// import HeroSection from '../astro-theme/components/HeroSection.astro';
+import Layout from './astro-theme/layouts/Layout';
+// import Testimonials from '../astro-theme/components/Testimonials.astro';
+// import CallToAction from '../astro-theme/components/CallToAction.astro';
+// import Blog from '../astro-theme/components/Blog.astro';
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
+
   return (
-    <Layout title={`AI 内容创作体验`} description="一块屏幕上打造极致的 AI 内容创作体验">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
+    <Layout title="Welcome to Astrolus.">
+      <main className="space-y-40 mb-40">
+        {/* <HeroSection />
+        <Features />
+        <Stats />
+        <Testimonials />
+        <CallToAction />
+        <Blog /> */}
       </main>
     </Layout>
   );
