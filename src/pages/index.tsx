22691e70a78010f47141bf82e045a9a85e277f33
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-08-14 00:48:00 +0200

    Demo 6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-// import Stats from '../astro-theme/components/Stats.astro';
+import Stats from './astro-theme/components/Stats';
 import Features from './astro-theme/components/Features';
 import HeroSection from './astro-theme/components/HeroSection';
 import Layout from './astro-theme/layouts/Layout';
-// import Testimonials from '../astro-theme/components/Testimonials.astro';
-// import CallToAction from '../astro-theme/components/CallToAction.astro';
+import Testimonials from './astro-theme/components/Testimonials';
+import CallToAction from './astro-theme/components/CallToAction';
 // import Blog from '../astro-theme/components/Blog.astro';
 
 export default function Home(): JSX.Element {
@@ -16,10 +16,10 @@ export default function Home(): JSX.Element {
       <main className="space-y-40 mb-40">
         <HeroSection />
         <Features />
-        {/* <Stats />
+        <Stats />
         <Testimonials />
         <CallToAction />
-        <Blog /> */}
+        {/* <Blog /> */}
       </main>
     </Layout>
   );
