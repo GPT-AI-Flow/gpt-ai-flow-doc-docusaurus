01808bf2941b7ab7f71b11e81537125f458cc210
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-08-14 14:29:35 +0200

    Fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -272,17 +272,6 @@ const config = {
       },
     ],
     'docusaurus-plugin-clarity',
-    async function myPlugin(context, options) {
-      return {
-        name: 'docusaurus-tailwindcss',
-        configurePostCss(postcssOptions) {
-          // Appends TailwindCSS and AutoPrefixer.
-          postcssOptions.plugins.push(require('tailwindcss'));
-          postcssOptions.plugins.push(require('autoprefixer'));
-          return postcssOptions;
-        },
-      };
-    },
   ],
 };
 
