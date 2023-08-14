47ca2f3b9a28e49ed798af95a0e90b14d9bc6c67
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-08-14 16:44:18 +0200

    Eject all Navbar

--- src/theme/Navbar/Layout/index.tsx
+++ src/theme/Navbar/Layout/index.tsx
@@ -1,41 +1,32 @@
-import React, { useEffect, type ComponentProps } from 'react';
+import React, {type ComponentProps} from 'react';
 import clsx from 'clsx';
-import { useThemeConfig } from '@docusaurus/theme-common';
-import { useHideableNavbar, useNavbarMobileSidebar } from '@docusaurus/theme-common/internal';
-import { translate } from '@docusaurus/Translate';
+import {useThemeConfig} from '@docusaurus/theme-common';
+import {
+  useHideableNavbar,
+  useNavbarMobileSidebar,
+} from '@docusaurus/theme-common/internal';
+import {translate} from '@docusaurus/Translate';
 import NavbarMobileSidebar from '@theme/Navbar/MobileSidebar';
-import type { Props } from '@theme/Navbar/Layout';
+import type {Props} from '@theme/Navbar/Layout';
 
 import styles from './styles.module.css';
 
 function NavbarBackdrop(props: ComponentProps<'div'>) {
-  return <div role="presentation" {...props} className={clsx('navbar-sidebar__backdrop', props.className)} />;
+  return (
+    <div
+      role="presentation"
+      {...props}
+      className={clsx('navbar-sidebar__backdrop', props.className)}
+    />
+  );
 }
 
-export default function NavbarLayout({ children }: Props): JSX.Element {
+export default function NavbarLayout({children}: Props): JSX.Element {
   const {
-    navbar: { hideOnScroll, style },
+    navbar: {hideOnScroll, style},
   } = useThemeConfig();
   const mobileSidebar = useNavbarMobileSidebar();
-  const { navbarRef, isNavbarVisible } = useHideableNavbar(hideOnScroll);
-
-  // === Add by xiaoka - start ===
-  const [scrollTop, setScrollTop] = React.useState(0);
-
-  useEffect(() => {
-    function handleScroll() {
-      const scrollTopValue = window.scrollY || document.documentElement.scrollTop;
-      setScrollTop(scrollTopValue);
-    }
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-  // === Add by xiaoka - end ===
-
+  const {navbarRef, isNavbarVisible} = useHideableNavbar(hideOnScroll);
   return (
     <nav
       ref={navbarRef}
@@ -47,17 +38,16 @@ export default function NavbarLayout({ children }: Props): JSX.Element {
       className={clsx(
         'navbar',
         'navbar--fixed-top',
-        hideOnScroll && [styles.navbarHideable, !isNavbarVisible && styles.navbarHidden],
+        hideOnScroll && [
+          styles.navbarHideable,
+          !isNavbarVisible && styles.navbarHidden,
+        ],
         {
           'navbar--dark': style === 'dark',
           'navbar--primary': style === 'primary',
           'navbar-sidebar--show': mobileSidebar.shown,
-        }
-      )}
-      // === Add by xiaoka - start ===
-      style={scrollTop <= 0 ? { background: 'transparent', boxShadow: 'none' } : {}}
-      // === Add by xiaoka - end ===
-    >
+        },
+      )}>
       {children}
       <NavbarBackdrop onClick={mobileSidebar.toggle} />
       <NavbarMobileSidebar />
