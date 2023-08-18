c4932ee6e3f1907a32cd812b9a370978d744bffc
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-08-18 22:57:20 +0200

    udpate downloadpage

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -28,7 +28,6 @@ const DownloadPage = (): JSX.Element => {
                 <div className="flex flex-wrap justify-center gap-4 md:justify-start lg:gap-6">
                   <a
                     href="https://github.com/GPT-AI-Flow/gpt-ai-flow-doc-docusaurus/releases/download/v0.1.19/GPT.AI.Flow-0.1.19.dmg"
-                    target="_blank"
                     title="Download for Mac"
                     aria-label="Download for Mac"
                     className="download-link-button"
@@ -42,7 +41,6 @@ const DownloadPage = (): JSX.Element => {
                   </a>
                   <a
                     href="https://github.com/GPT-AI-Flow/gpt-ai-flow-doc-docusaurus/releases/download/v0.1.19/GPT.AI.Flow-0.1.19-arm64.dmg"
-                    target="_blank"
                     title="Download for Mac"
                     aria-label="Download for Mac"
                     className="download-link-button"
@@ -56,7 +54,6 @@ const DownloadPage = (): JSX.Element => {
                   </a>
                   <a
                     href="https://github.com/GPT-AI-Flow/gpt-ai-flow-doc-docusaurus/releases/download/v0.1.19/GPT.AI.Flow.Setup.0.1.19.exe"
-                    target="_blank"
                     title="Download for Windows"
                     aria-label="Download for Windows"
                     className="download-link-button"
