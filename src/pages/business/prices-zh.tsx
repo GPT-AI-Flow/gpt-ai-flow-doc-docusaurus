bf0c8a7b7dcbafe60d4e247e9fb91b39f0e03021
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-09-15 17:13:36 +0200

    Create prices-zh page

--- src/pages/business/prices-zh.tsx
+++ src/pages/business/prices-zh.tsx
@@ -2,7 +2,7 @@ import './prices.css';
 
 import React, { useEffect } from 'react';
 
-function PricingPage() {
+function PricingZHPage() {
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://js.stripe.com/v3/pricing-table.js';
@@ -18,20 +18,20 @@ function PricingPage() {
     <div className="business_prices_page_contianer">
       {/* Live */}
       <div style={{ backgroundColor: '#3f51b5' }}>
-        <h2 style={{ color: '#fff', marginTop: '2rem', textAlign: 'center' }}>欧元区</h2>
-        {/* @ts-ignore: Live-EUR */}
+        <h2 style={{ color: '#fff', marginTop: '2rem', textAlign: 'center' }}>人民币区</h2>
+        {/* @ts-ignore: Live-CNY */}
         <stripe-pricing-table
-          pricing-table-id="prctbl_1NhbvGDfpKDtUNgncj7TTmDN"
+          pricing-table-id="prctbl_1NjP6PDfpKDtUNgnsHRJF2SH"
           publishable-key="pk_live_51HqdzoDfpKDtUNgnMQxyO3NPLAxBb1IS53DE9YXgmV9w5rEwsAOBaohqyL7bNTVjZQ8N4Ry0R9XXDIW9q0aMZFk100OtkFDHRx"
         />
       </div>
 
       {/* Test */}
       <div style={{ display: 'none', backgroundColor: '#3f51b5' }}>
-        <h2 style={{ color: '#fff', marginTop: '2rem', textAlign: 'center' }}>欧元区(测试)</h2>
-        {/* @ts-ignore: Test-EUR */}
+        <h2 style={{ color: '#fff', marginTop: '2rem', textAlign: 'center' }}>人民币区(测试)</h2>
+        {/* @ts-ignore: Test-CNY */}
         <stripe-pricing-table
-          pricing-table-id="prctbl_1NhXXMDfpKDtUNgnT5a7WDKY"
+          pricing-table-id="prctbl_1NmL5mDfpKDtUNgnWvIvzUUW"
           publishable-key="pk_test_51HqdzoDfpKDtUNgns7gfUm2rLbJAqgKVTiJVBpkX2ViLKkmLiV11eXasYcJjUsj6ARpDhju4yAFMiWAEl3ynE2T100S1mBLctz"
         />
       </div>
@@ -39,4 +39,4 @@ function PricingPage() {
   );
 }
 
-export default PricingPage;
+export default PricingZHPage;
