ec3740704885bf0f9b433bea2cf86fbaeb87528b
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-09-16 18:34:34 +0200

    Use new payment for zh

--- src/pages/business/prices-zh.tsx
+++ src/pages/business/prices-zh.tsx
@@ -19,9 +19,16 @@ function PricingZHPage() {
       {/* Live */}
       <div style={{ backgroundColor: '#3f51b5' }}>
         <h2 style={{ color: '#fff', marginTop: '2rem', textAlign: 'center' }}>人民币区</h2>
+        <h3 style={{ color: '#fff', marginTop: '2rem', textAlign: 'center' }}>入门工具版</h3>
         {/* @ts-ignore: Live-CNY */}
         <stripe-pricing-table
-          pricing-table-id="prctbl_1NjP6PDfpKDtUNgnsHRJF2SH"
+          pricing-table-id="prctbl_1NqfnyDfpKDtUNgnXOyVxQRo"
+          publishable-key="pk_live_51HqdzoDfpKDtUNgnMQxyO3NPLAxBb1IS53DE9YXgmV9w5rEwsAOBaohqyL7bNTVjZQ8N4Ry0R9XXDIW9q0aMZFk100OtkFDHRx"
+        />
+        <h3 style={{ color: '#fff', marginTop: '2rem', textAlign: 'center' }}>入门模型版</h3>
+        {/* @ts-ignore: Live-CNY */}
+        <stripe-pricing-table
+          pricing-table-id="prctbl_1NqfozDfpKDtUNgnz729Wrp4"
           publishable-key="pk_live_51HqdzoDfpKDtUNgnMQxyO3NPLAxBb1IS53DE9YXgmV9w5rEwsAOBaohqyL7bNTVjZQ8N4Ry0R9XXDIW9q0aMZFk100OtkFDHRx"
         />
       </div>
