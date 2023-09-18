ac67be1c970c133989d3b6b70669d844c14df21a
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-09-18 17:39:20 +0200

    Create src/pages/testimonial.tsx

---
+++ src/pages/testimonial.tsx
@@ -0,0 +1,156 @@
+import Layout from '@theme/Layout';
+import React from 'react';
+
+const TestimonialPage = (): JSX.Element => {
+  return (
+    <Layout
+      title={`我们真实的用户评价`}
+      description="面对自媒体内容创作的挑战, GPT AI Flow 是您的最佳助手。适合新手到专业博主，我们帮您生成高质量文案、优化互动并确保内容被搜索引擎发现。加入我们，共创成功故事！"
+    >
+      <div className="Container" id="testimonialPage">
+        <div className="py-16">
+          <div className="container m-auto px-6 text-gray-600 dark:text-gray-300 md:px-12 xl:px-6">
+            <div className="mb-20 space-y-4 px-6 md:px-0">
+              <h2 className="text-center text-2xl font-bold text-gray-800 dark:text-white md:text-4xl">
+                We have some fans.
+              </h2>
+            </div>
+            <div className="md:columns-2 lg:columns-3 gap-8 space-y-8">
+              <div className="aspect-auto p-8 border border-gray-100 rounded-3xl bg-white dark:bg-gray-800 dark:border-gray-700 shadow-2xl shadow-gray-600/10 dark:shadow-none">
+                <div className="flex gap-4">
+                  <img
+                    className="w-12 h-12 rounded-full"
+                    src="https://pbs.twimg.com/profile_images/1559671811780087810/Y4E8X2do_400x400.jpg"
+                    alt=""
+                    width=""
+                    height=""
+                    loading="lazy"
+                  />
+                  <div>
+                    <h6 className="text-lg font-medium text-gray-700 dark:text-white">Eric Ampire</h6>
+                    <p className="text-sm text-gray-500 dark:text-gray-400">Mobile dev</p>
+                  </div>
+                </div>
+                <p className="mt-8">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum aliquid quo eum quae quos illo earum
+                  ipsa doloribus nostrum minus libero aspernatur laborum cum, a suscipit, ratione ea totam ullam! Lorem
+                  ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam
+                  sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum
+                  et rerum.
+                </p>
+              </div>
+              <div className="aspect-auto p-8 border border-gray-100 rounded-3xl bg-white dark:bg-gray-800 dark:border-gray-700 shadow-2xl shadow-gray-600/10 dark:shadow-none">
+                <div className="flex gap-4">
+                  <img
+                    className="w-12 h-12 rounded-full"
+                    src="https://pbs.twimg.com/profile_images/1372441824560771075/DTcuXT0Z_400x400.jpg"
+                    alt=""
+                    width=""
+                    height=""
+                    loading="lazy"
+                  />
+                  <div>
+                    <h6 className="text-lg font-medium text-gray-700 dark:text-white">Rodrigo Aguilar</h6>
+                    <p className="text-sm text-gray-500 dark:text-gray-400">Creator of Tailwind Awesome</p>
+                  </div>
+                </div>
+                <p className="mt-8">
+                  {' '}
+                  Lorem ipsum dolor laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint
+                  libero voluptas veniam at reprehenderit, veritatis harum et rerum.
+                </p>
+              </div>
+              <div className="aspect-auto p-8 border border-gray-100 rounded-3xl bg-white dark:bg-gray-800 dark:border-gray-700 shadow-2xl shadow-gray-600/10 dark:shadow-none">
+                <div className="flex gap-4">
+                  <img
+                    className="w-12 h-12 rounded-full"
+                    src="https://pbs.twimg.com/profile_images/1362367807887974401/kuJ1OFT1_400x400.jpg"
+                    alt=""
+                    width=""
+                    height=""
+                    loading="lazy"
+                  />
+                  <div>
+                    <h6 className="text-lg font-medium text-gray-700 dark:text-white">Adam Wathan</h6>
+                    <p className="text-sm text-gray-500 dark:text-gray-400">Creator of Tailwindcss</p>
+                  </div>
+                </div>
+                <p className="mt-8">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab
+                  veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit,
+                  veritatis harum et rerum.
+                </p>
+              </div>
+              <div className="aspect-auto p-8 border border-gray-100 rounded-3xl bg-white dark:bg-gray-800 dark:border-gray-700 shadow-2xl shadow-gray-600/10 dark:shadow-none">
+                <div className="flex gap-4">
+                  <img
+                    className="w-12 h-12 rounded-full"
+                    src="https://pbs.twimg.com/profile_images/1511686703672606725/_DV4Mfjd_400x400.jpg"
+                    alt=""
+                    width=""
+                    height=""
+                    loading="lazy"
+                  />
+                  <div>
+                    <h6 className="text-lg font-medium text-gray-700 dark:text-white">Dan Kyungu</h6>
+                    <p className="text-sm text-gray-500 dark:text-gray-400">Mobile dev</p>
+                  </div>
+                </div>
+                <p className="mt-8">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab
+                  veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit,
+                  veritatis harum et rerum.
+                </p>
+              </div>
+              <div className="aspect-auto p-8 border border-gray-100 rounded-3xl bg-white dark:bg-gray-800 dark:border-gray-700 shadow-2xl shadow-gray-600/10 dark:shadow-none">
+                <div className="flex gap-4">
+                  <img
+                    className="w-12 h-12 rounded-full"
+                    src="images/first_user.webp"
+                    alt=""
+                    width=""
+                    height=""
+                    loading="lazy"
+                  />
+                  <div>
+                    <h6 className="text-lg font-medium text-gray-700 dark:text-white">John Doe</h6>
+                    <p className="text-sm text-gray-500 dark:text-gray-400">Creator dev</p>
+                  </div>
+                </div>
+                <p className="mt-8">
+                  {' '}
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab
+                  veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit,
+                  veritatis harum et rerum.
+                </p>
+              </div>
+              <div className="aspect-auto p-8 border border-gray-100 rounded-3xl bg-white dark:bg-gray-800 dark:border-gray-700 shadow-2xl shadow-gray-600/10 dark:shadow-none">
+                <div className="flex gap-4">
+                  <img
+                    className="w-12 h-12 rounded-full"
+                    src="images/second_user.webp"
+                    alt=""
+                    width=""
+                    height=""
+                    loading="lazy"
+                  />
+                  <div>
+                    <h6 className="text-lg font-medium text-gray-700 dark:text-white">Randy Doe</h6>
+                    <p className="text-sm text-gray-500 dark:text-gray-400">Mobile dev</p>
+                  </div>
+                </div>
+                <p className="mt-8">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab
+                  veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit,
+                  veritatis harum et rerum.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default TestimonialPage;
