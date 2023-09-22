ef75d20b0192b596c25e6c38fb37d9f718e7cbad
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-09-23 00:07:23 +0200

    update

--- src/pages/astro-theme/components/Features.tsx
+++ src/pages/astro-theme/components/Features.tsx
@@ -39,9 +39,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
 
           <a
             href={
-              isHomePage
-                ? '/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'
-                : '/docs/application-scenarios/praiseAssistant'
+              isHomePage ? '/docs/application-scenarios/introduction' : '/docs/application-scenarios/praiseAssistant'
             }
             className="flex items-center justify-between group-hover:text-secondary"
             style={{ textDecoration: 'none' }}
@@ -89,9 +87,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
 
           <a
             href={
-              isHomePage
-                ? '/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'
-                : '/docs/application-scenarios/responseToLeander'
+              isHomePage ? '/docs/application-scenarios/introduction' : '/docs/application-scenarios/responseToLeander'
             }
             className="flex items-center justify-between group-hover:text-secondary"
             style={{ textDecoration: 'none' }}
@@ -137,11 +133,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
             </p>
           </div>
           <a
-            href={
-              isHomePage
-                ? '/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'
-                : '/docs/application-scenarios/pengYouQuan'
-            }
+            href={isHomePage ? '/docs/application-scenarios/introduction' : '/docs/application-scenarios/pengYouQuan'}
             className="flex items-center justify-between group-hover:text-secondary"
             style={{ textDecoration: 'none' }}
           >
@@ -185,27 +177,26 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
               ✒️ 提供邮件写作建议，<b>确保您的邮件内容既专业又得体</b>
             </p>
           </div>
-          {isHomePage && (
-            <a
-              href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
-              className="flex items-center justify-between group-hover:text-secondary"
-              style={{ textDecoration: 'none' }}
+
+          <a
+            href={isHomePage ? '/docs/application-scenarios/introduction' : '/docs/application-scenarios/email'}
+            className="flex items-center justify-between group-hover:text-secondary"
+            style={{ textDecoration: 'none' }}
+          >
+            <span className="text-sm">了解更多</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
             >
-              <span className="text-sm">了解更多</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </a>
-          )}
+              <path
+                fillRule="evenodd"
+                d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
         </div>
       </div>
       <div className="group relative bg-white dark:bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
@@ -234,7 +225,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
           </div>
           {isHomePage && (
             <a
-              href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
+              href={'/docs/application-scenarios/introduction'}
               className="flex items-center justify-between group-hover:text-secondary"
               style={{ textDecoration: 'none' }}
             >
@@ -281,7 +272,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
           </div>
           {isHomePage && (
             <a
-              href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
+              href={'/docs/application-scenarios/introduction'}
               className="flex items-center justify-between group-hover:text-secondary"
               style={{ textDecoration: 'none' }}
             >
@@ -328,7 +319,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
           </div>
           {isHomePage && (
             <a
-              href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
+              href={'/docs/application-scenarios/introduction'}
               className="flex items-center justify-between group-hover:text-secondary"
               style={{ textDecoration: 'none' }}
             >
@@ -424,7 +415,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
               </div>
               {isHomePage && (
                 <a
-                  href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
+                  href={'/docs/application-scenarios/introduction'}
                   className="flex items-center justify-between group-hover:text-secondary"
                   style={{ textDecoration: 'none' }}
                 >
@@ -471,7 +462,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
               </div>
               {isHomePage && (
                 <a
-                  href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
+                  href={'/docs/application-scenarios/introduction'}
                   className="flex items-center justify-between group-hover:text-secondary"
                   style={{ textDecoration: 'none' }}
                 >
@@ -518,7 +509,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
               </div>
               {isHomePage && (
                 <a
-                  href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
+                  href={'/docs/application-scenarios/introduction'}
                   className="flex items-center justify-between group-hover:text-secondary"
                   style={{ textDecoration: 'none' }}
                 >
@@ -565,7 +556,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
               </div>
               {isHomePage && (
                 <a
-                  href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
+                  href={'/docs/application-scenarios/introduction'}
                   className="flex items-center justify-between group-hover:text-secondary"
                   style={{ textDecoration: 'none' }}
                 >
@@ -612,7 +603,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
               </div>
               {isHomePage && (
                 <a
-                  href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
+                  href={'/docs/application-scenarios/introduction'}
                   className="flex items-center justify-between group-hover:text-secondary"
                   style={{ textDecoration: 'none' }}
                 >
@@ -659,7 +650,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
               </div>
               {isHomePage && (
                 <a
-                  href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
+                  href={'/docs/application-scenarios/introduction'}
                   className="flex items-center justify-between group-hover:text-secondary"
                   style={{ textDecoration: 'none' }}
                 >
@@ -706,7 +697,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
               </div>
               {isHomePage && (
                 <a
-                  href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
+                  href={'/docs/application-scenarios/introduction'}
                   className="flex items-center justify-between group-hover:text-secondary"
                   style={{ textDecoration: 'none' }}
                 >
@@ -753,7 +744,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
               </div>
               {isHomePage && (
                 <a
-                  href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
+                  href={'/docs/application-scenarios/introduction'}
                   className="flex items-center justify-between group-hover:text-secondary"
                   style={{ textDecoration: 'none' }}
                 >
@@ -800,7 +791,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
               </div>
               {isHomePage && (
                 <a
-                  href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
+                  href={'/docs/application-scenarios/introduction'}
                   className="flex items-center justify-between group-hover:text-secondary"
                   style={{ textDecoration: 'none' }}
                 >
@@ -847,7 +838,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
               </div>
               {isHomePage && (
                 <a
-                  href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
+                  href={'/docs/application-scenarios/introduction'}
                   className="flex items-center justify-between group-hover:text-secondary"
                   style={{ textDecoration: 'none' }}
                 >
@@ -894,7 +885,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
               </div>
               {isHomePage && (
                 <a
-                  href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
+                  href={'/docs/application-scenarios/introduction'}
                   className="flex items-center justify-between group-hover:text-secondary"
                   style={{ textDecoration: 'none' }}
                 >
@@ -941,7 +932,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
               </div>
               {isHomePage && (
                 <a
-                  href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
+                  href={'/docs/application-scenarios/introduction'}
                   className="flex items-center justify-between group-hover:text-secondary"
                   style={{ textDecoration: 'none' }}
                 >
@@ -988,7 +979,7 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
               </div>
               {isHomePage && (
                 <a
-                  href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
+                  href={'/docs/application-scenarios/introduction'}
                   className="flex items-center justify-between group-hover:text-secondary"
                   style={{ textDecoration: 'none' }}
                 >
