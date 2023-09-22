0e71bf7cd8a58b54edfbd5b8e534de5f51346418
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-09-22 23:16:52 +0200

    update

--- src/pages/astro-theme/components/Features.tsx
+++ src/pages/astro-theme/components/Features.tsx
@@ -36,27 +36,30 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
               🎁 适用于亲友、同事等，让他们感受到您的<b>认可和鼓励</b>
             </p>
           </div>
-          {isHomePage && (
-            <a
-              href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
-              className="flex items-center justify-between group-hover:text-secondary"
-              style={{ textDecoration: 'none' }}
+
+          <a
+            href={
+              isHomePage
+                ? '/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'
+                : '/docs/application-scenarios/praiseAssistant'
+            }
+            className="flex items-center justify-between group-hover:text-secondary"
+            style={{ textDecoration: 'none' }}
+          >
+            <span className="text-sm">了解更多</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
             >
-              <span className="text-sm">了解更多</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </a>
-          )}
+              <path
+                fillRule="evenodd"
+                d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
         </div>
       </div>
       <div className="group relative bg-white dark:bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
@@ -83,27 +86,30 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
               🤝 帮助你与上司<b>建立更和谐的工作关系</b>
             </p>
           </div>
-          {isHomePage && (
-            <a
-              href={'/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'}
-              className="flex items-center justify-between group-hover:text-secondary"
-              style={{ textDecoration: 'none' }}
+
+          <a
+            href={
+              isHomePage
+                ? '/docs/proudct/how-professional-mode-of-ai-flow-revolutionizes-content-creation'
+                : '/docs/application-scenarios/responseToLeander'
+            }
+            className="flex items-center justify-between group-hover:text-secondary"
+            style={{ textDecoration: 'none' }}
+          >
+            <span className="text-sm">了解更多</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
             >
-              <span className="text-sm">了解更多</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </a>
-          )}
+              <path
+                fillRule="evenodd"
+                d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
         </div>
       </div>
       <div className="group relative bg-white dark:bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
