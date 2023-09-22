a45b1cdbefbdad311826923188de226a21e16ce1
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-09-22 21:51:29 +0200

    up

--- src/pages/astro-theme/components/CallToAction.tsx
+++ src/pages/astro-theme/components/CallToAction.tsx
@@ -72,7 +72,7 @@ const CallToAction = () => {
                 <span className="relative text-base font-semibold text-white dark:text-dark">开始创作</span>
               </a>
               <a
-                href="/docs/proudct/basic-presentation" // 产品模块介绍
+                href="/docs/intro/AI-Flow-Supercharge-Your-Self-Media-Contents"
                 className="learn-more-button relative flex h-12 w-full items-center justify-center px-8 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-primary/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800 sm:w-max"
                 style={{ textDecoration: 'none' }}
               >
