752a80b32eaf8e67d1f1c8af154120b7b68197c5
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-09-23 12:06:22 +0200

    dsfs

--- src/pages/astro-theme/components/Features.tsx
+++ src/pages/astro-theme/components/Features.tsx
@@ -270,27 +270,28 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
               ✍️ 为会议、项目、活动等提供<b>专业的文字总结模板</b>，助您提高工作效率
             </p>
           </div>
-          {isHomePage && (
-            <a
-              href={'/docs/application-scenarios/introduction'}
-              className="flex items-center justify-between group-hover:text-secondary"
-              style={{ textDecoration: 'none' }}
+
+          <a
+            href={
+              isHomePage ? '/docs/application-scenarios/introduction' : '/docs/application-scenarios/summaryAssitant'
+            }
+            className="flex items-center justify-between group-hover:text-secondary"
+            style={{ textDecoration: 'none' }}
+          >
+            <span className="text-sm">了解更多</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
             >
-              <span className="text-sm">了解更多</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </a>
-          )}
+              <path
+                fillRule="evenodd"
+                d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
         </div>
       </div>
       <div className="group relative bg-white dark:bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
