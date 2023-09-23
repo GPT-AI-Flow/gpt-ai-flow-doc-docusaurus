512149521426eb819a22e052d3a1f973ee4c8b6d
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-09-23 13:56:44 +0200

    iudpate

--- src/pages/astro-theme/components/Features.tsx
+++ src/pages/astro-theme/components/Features.tsx
@@ -980,27 +980,29 @@ export const CardsForFeatures = (props: CardsForFeatures_input) => {
                   🚀 请持续关注，<b>与我们一起走进未来</b>!
                 </p>
               </div>
-              {isHomePage && (
-                <a
-                  href={'/docs/application-scenarios/introduction'}
-                  className="flex items-center justify-between group-hover:text-secondary"
-                  style={{ textDecoration: 'none' }}
+              <a
+                href={
+                  isHomePage
+                    ? '/docs/application-scenarios/introduction'
+                    : '/docs/proudct/user-guide-floating-window-commands-management'
+                }
+                className="flex items-center justify-between group-hover:text-secondary"
+                style={{ textDecoration: 'none' }}
+              >
+                <span className="text-sm">了解更多</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
                 >
-                  <span className="text-sm">了解更多</span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </a>
-              )}
+                  <path
+                    fillRule="evenodd"
+                    d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
             </div>
           </div>
         </>
