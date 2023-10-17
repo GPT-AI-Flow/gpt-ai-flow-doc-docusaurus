296f0f4a7e4c8cf5befd18a3d9c990850f71f86e
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-10-17 18:43:35 +0200

    Create social-media-assistant page

---
+++ src/pages/product/social-media-assistant/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Layout from '@theme/Layout';
+
+const SocialMediaAssistantPage = () => {
+  return (
+    <Layout
+      title={`自媒体运营助手`}
+      description="面对自媒体内容创作的挑战, GPT AI Flow 是您的最佳助手。适合新手到专业博主，我们帮您生成高质量文案、优化互动并确保内容被搜索引擎发现。加入我们，共创成功故事！"
+    >
+      <div>Social Media Assistant Page</div>
+    </Layout>
+  );
+};
+
+export default SocialMediaAssistantPage;
