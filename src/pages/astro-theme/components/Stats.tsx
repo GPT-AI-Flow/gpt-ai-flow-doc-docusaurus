7e964d0fd386bebc9ae038e6c845a9d14f158d22
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-10-17 20:39:13 +0200

    Update images

--- src/pages/astro-theme/components/Stats.tsx
+++ src/pages/astro-theme/components/Stats.tsx
@@ -26,7 +26,15 @@ const Stats = () => {
 
         <div className="space-y-6 justify-between text-gray-600 md:flex flex-row-reverse md:gap-6 md:space-y-0 lg:gap-12 lg:items-center">
           <div className="md:5/12 lg:w-1/2">
-            <img src="/img/astro-theme/pie.svg" alt="image" loading="lazy" width="" height="" className="w-full" />
+            {/* <img src="/img/astro-theme/pie.svg" alt="image" loading="lazy" width="" height="" className="w-full" /> */}
+            <img
+              src="/img/pages/homePage/2023-10-17-img-3-homePage-two-feature-presentation.gif"
+              alt="image"
+              loading="lazy"
+              width=""
+              height=""
+              className="w-full rounded-lg shadow-xl"
+            />
           </div>
           <div className="md:7/12 lg:w-1/2">
             <h2 className="text-3xl font-bold text-gray-900 md:text-4xl dark:text-white">
