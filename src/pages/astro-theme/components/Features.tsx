cf6860603eb6abea204590f8e41ea2a5ac6f868e
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-10-19 18:55:35 +0200

    uipdate

--- src/pages/astro-theme/components/Features.tsx
+++ src/pages/astro-theme/components/Features.tsx
@@ -11,34 +11,6 @@ interface IOneFeature {
   openLink: string;
 }
 
-const Card_without_click = (props: { item: IOneFeature }) => {
-  const {
-    item: { icon, proModeModuleName, featureText_1, featureText_2, featureText_3 },
-  } = props;
-
-  return (
-    <div className="group relative bg-white dark:bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
-      <div className="relative space-y-8 py-12 p-8">
-        <img
-          src={icon}
-          className="w-12"
-          // width="512"
-          // height="512"
-          alt="burger illustration"
-        />
-
-        <div className="space-y-2">
-          <h5 className="text-xl font-semibold text-gray-700 dark:text-white transition group-hover:text-secondary">
-            {proModeModuleName}
-          </h5>
-          <p className="text-gray-600 dark:text-gray-300">{featureText_1}</p>
-          <p className="text-gray-600 dark:text-gray-300">{featureText_2}</p>
-          <p className="text-gray-600 dark:text-gray-300">{featureText_3}</p>
-        </div>
-      </div>
-    </div>
-  );
-};
 const Card_with_click = (props: { item: IOneFeature }) => {
   const {
     item: { icon, proModeModuleName, featureText_1, featureText_2, featureText_3, openLink },
@@ -83,6 +55,34 @@ const Card_with_click = (props: { item: IOneFeature }) => {
           )}
         </div>
       </a>
+    </div>
+  );
+};
+const Card_without_click = (props: { item: IOneFeature }) => {
+  const {
+    item: { icon, proModeModuleName, featureText_1, featureText_2, featureText_3 },
+  } = props;
+
+  return (
+    <div className="group relative bg-white dark:bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
+      <div className="relative space-y-8 py-12 p-8">
+        <img
+          src={icon}
+          className="w-12"
+          // width="512"
+          // height="512"
+          alt="burger illustration"
+        />
+
+        <div className="space-y-2">
+          <h5 className="text-xl font-semibold text-gray-700 dark:text-white transition group-hover:text-secondary">
+            {proModeModuleName}
+          </h5>
+          <p className="text-gray-600 dark:text-gray-300">{featureText_1}</p>
+          <p className="text-gray-600 dark:text-gray-300">{featureText_2}</p>
+          <p className="text-gray-600 dark:text-gray-300">{featureText_3}</p>
+        </div>
+      </div>
     </div>
   );
 };
