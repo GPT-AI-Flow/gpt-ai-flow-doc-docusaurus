1132a2b5a8d374ae20598a3ec538e65d5b80af1a
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-11-03 21:44:05 +0100

    Updat

--- src/pages/astro-theme/components/HeroSection.tsx
+++ src/pages/astro-theme/components/HeroSection.tsx
@@ -21,7 +21,7 @@ const HeroSection = () => {
             </p>
             <div className="mt-16 flex flex-wrap justify-center gap-y-4 gap-x-6">
               <a
-                href="/docs/proudct/how-to-start-a-free-trial-of-gpt-ai-flow"
+                href="/download"
                 className="get-start-button relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
                 style={{ textDecoration: 'none' }}
               >
