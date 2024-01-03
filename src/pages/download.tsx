e7c7db1d687fc0a4d697e859f37ca4f4ada7d59c
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-01-03 15:48:17 +0100

    aa

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -88,7 +88,7 @@ const DownloadPage = (): JSX.Element => {
                 <div className="text-sm mt-2">
                   已经有{' '}
                   <span className="text-info dark:text-secondary">
-                    <b>150+</b>
+                    <b>160+</b>
                   </span>{' '}
                   用户注册使用
                 </div>
