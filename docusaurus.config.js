301e12d455317a38ad9a4dc6a5ca1bd3861a3ab2
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-01-15 16:18:41 +0100

    Test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -316,7 +316,12 @@ const config = {
     [
       'docusaurus2-dotenv',
       {
+        // path: './.env', // The path to your environment variables.
+        // safe: false, // If false ignore safe-mode, if true load './.env.example', if a string load that file as the sample
         systemvars: true, // Set to true if you would rather load all system variables as well (useful for CI purposes)
+        // silent: false, //  If true, all warnings will be suppressed
+        // expand: false, // Allows your variables to be "expanded" for reusability within your .env file
+        // defaults: false, //  Adds support for dotenv-defaults. If set to true, uses ./.env.defaults
       },
     ],
     [
@@ -344,6 +349,11 @@ const config = {
       };
     },
   ],
+
+  customFields: {
+    APP_ENV: process.env.APP_ENV,
+    abc: 'yang',
+  },
 };
 
 module.exports = config;
--- src/pages/business/prices.tsx
+++ src/pages/business/prices.tsx
@@ -1,8 +1,12 @@
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import './prices.css';
 
 import React, { useEffect } from 'react';
 
 function PricingPage() {
+  const { siteConfig } = useDocusaurusContext();
+  console.log('siteConfig', siteConfig);
+
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://js.stripe.com/v3/pricing-table.js';
@@ -16,6 +20,7 @@ function PricingPage() {
 
   return (
     <div className="business_prices_page_contianer">
+      return <div>{`${(siteConfig as any)?.APP_ENV}`}</div>;
       <div className="row" style={{ backgroundColor: '#3f51b5' }}>
         <a href="/business/prices-zh" style={{ margin: 'auto' }}>
           <h2 style={{ color: '#fff', textAlign: 'center', marginTop: '2rem' }}>
@@ -23,7 +28,6 @@ function PricingPage() {
           </h2>
         </a>
       </div>
-
       {/* Live */}
       <div style={{ backgroundColor: '#3f51b5' }}>
         <h2 style={{ color: '#fff', marginTop: '2rem', textAlign: 'center' }}>欧元区</h2>
@@ -33,7 +37,6 @@ function PricingPage() {
           publishable-key="pk_live_51HqdzoDfpKDtUNgnMQxyO3NPLAxBb1IS53DE9YXgmV9w5rEwsAOBaohqyL7bNTVjZQ8N4Ry0R9XXDIW9q0aMZFk100OtkFDHRx"
         />
       </div>
-
       {/* Test */}
       <div style={{ display: 'none', backgroundColor: '#3f51b5' }}>
         <h2 style={{ color: '#fff', marginTop: '2rem', textAlign: 'center' }}>欧元区(测试)</h2>
